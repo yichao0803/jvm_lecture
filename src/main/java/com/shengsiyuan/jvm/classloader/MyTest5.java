bca02a71de7d202946c6d98579986d6568c1ae8b
ZhangYichao <yichao0803@qq.com>
2019-07-27 10:24:37 +0800

    增加Test6.java文件；本示例演示了 类的初始化步骤中③：假如类中有初始化语句，则系统依次执行这些初始化语句

---
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest5.java
@@ -0,0 +1,10 @@
+package com.shengsiyuan.jvm.classloader;
+
+/**
+ * @Classname MyTest5
+ * @Description TODO
+ * @Date 2019/7/27 10:06
+ * @Created by Zhangyichao
+ */
+public class MyTest5 {
+}
---
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest6.java
@@ -0,0 +1,41 @@
+package com.shengsiyuan.jvm.classloader;
+
+/**
+ * @Classname MyTest6
+ * @Description
+ * 3、初始化
+ * JVM初始化步骤
+ * ① 假如这个类还没有被加载和连接，则程序先加载并连接该类
+ * ② 假如该类的直接父类还没有被初始化，则先初始化其直接父类
+ * ③ 假如类中有初始化语句，则系统依次执行这些初始化语句
+ *
+ * 本示例演示了 类的初始化步骤中③：假如类中有初始化语句，则系统依次执行这些初始化语句
+ *
+ * @Date 2019/7/27 10:07
+ * @Created by Zhangyichao
+ */
+public class MyTest6 {
+    public static void main(String[] args) {
+        Singleton singleton=Singleton.getInstance();
+        System.out.println("Singleton.counter1："+Singleton.counter1);
+        System.out.println("Singleton.counter2："+Singleton.counter2);
+    }
+}
+
+class Singleton{
+    public static int counter1=1;
+    public static Singleton singleton=new Singleton();
+    public static int counter2=0;
+    private Singleton(){
+        counter1++;
+        counter2++;
+        System.out.println("counter1："+counter1);
+        System.out.println("counter2："+counter2);
+        System.out.println("*********");
+    }
+
+    public static  Singleton getInstance(){
+        return  singleton;
+    }
+
+}
