3920aa898634d06d5c6144311b022e14fa339671
ZhangYichao <yichao0803@qq.com>
2019-07-27 10:38:26 +0800

    2、连接（2）准备 为类的静态变量分配内存，并将其初始化为默认值 ；注意事项第三条 ③ 如果类字段的字段属性表中存在ConstantValue属性，即同时被final和static修饰，那么在准备阶段变量value就会被初始化为ConstantValue属性所指定的值。假设上面的类变量value被定义为： public static final int value = 3；编译时Javac将会为value生成ConstantValue属性，在准备阶段虚拟机就会根据ConstantValue的设置将value赋值为3。我们可以理解为static final常量在编译期就将其结果放入了调用它的类的常量池中

--- src/main/java/com/shengsiyuan/jvm/classloader/MyTest2.java
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest2.java
@@ -3,6 +3,15 @@ package com.shengsiyuan.jvm.classloader;
 /**
  * @Classname MyTest4
  * @Description
+ * 2、连接
+ * （2）准备 为类的静态变量分配内存，并将其初始化为默认值
+ * 注意事项第三条
+ * ③ 如果类字段的字段属性表中存在ConstantValue属性，即同时被final和static修饰，
+ * 那么在准备阶段变量value就会被初始化为ConstantValue属性所指定的值。
+ * 假设上面的类变量value被定义为： public static final int value = 3；
+ * 编译时Javac将会为value生成ConstantValue属性，在准备阶段虚拟机就会根据ConstantValue的设置将value赋值为3。
+ * 我们可以理解为static final常量在编译期就将其结果放入了调用它的类的常量池中
+ * **********************************************************************
  *  常量在编译阶段会存入到调用这个常量的方法所在的类的常量池中。
  *  本质上，调用类并并没有直接引用到定义常量的类，因此不会触发
  *  定义常量的类的初始化
