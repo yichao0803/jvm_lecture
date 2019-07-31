b25ca0e9c3616b21ff3b7270abac13a95dd702a1
ZhangYichao <yichao0803@qq.com>
2019-07-31 23:06:26 +0800

    增加MyTest10

---
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest10.java
@@ -0,0 +1,35 @@
+package com.shengsiyuan.jvm.classloader;
+
+/**
+ * @Classname MyTest10
+ * @Description TODO
+ * @Date 2019/7/31 22:59
+ * @Created by Zhangyichao
+ */
+
+class Parent2{
+    static  int a=3;
+    static {
+        System.out.println("Parent2 static block");
+    }
+}
+
+class  Child2{
+    static int b =4;
+    static {
+        System.out.println("Child2 static block");
+    }
+}
+
+public class MyTest10 {
+    public static void main(String[] args) {
+        Parent2 parent2;
+        String str="--------------";
+        System.out.println(str);
+        parent2=new Parent2();
+        System.out.println(str);
+        System.out.println(Parent2.a);
+        System.out.println(str);
+        System.out.println(Child2.b);
+    }
+}
